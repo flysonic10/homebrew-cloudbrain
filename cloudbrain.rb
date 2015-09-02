6b772876b0cac8e7dfe525059b3a38359bb62b0a
William Wnekowicz <william.wnekowicz@gmail.com>
2015-09-02 01:28:52 -0700

    Cloudbrain 0.2.1

--- cloudbrain.rb
+++ cloudbrain.rb
@@ -1,8 +1,8 @@
 class Cloudbrain < Formula
   desc "Platform for real-time sensor data analysis and visualization."
   homepage "http://demo.cloudbrain.rocks"
-  url "https://pypi.python.org/packages/source/c/cloudbrain/cloudbrain-0.2.0.tar.gz"
-  sha256 "0e7832bb161d2a4d694c2cc39c5c17e9ebb5d6e5da86d6eb1ab4acb9feff02aa"
+  url "https://pypi.python.org/packages/source/c/cloudbrain/cloudbrain-0.2.1.tar.gz"
+  sha256 "0780d28ebb32f0759d56f1ed39629c977a23640ff0c952479593fbc9b72c65a4"
 
   depends_on :python if MacOS.version <= :snow_leopard
   depends_on "liblo"
@@ -17,10 +17,15 @@ class Cloudbrain < Formula
     sha256 "382ee7360aa00aeebf1b955eef65f8491366657a626254574c647521b36e0eb0"
   end
 
+  resource "pyserial" do
+    url "https://pypi.python.org/packages/source/p/pyserial/pyserial-2.7.tar.gz"
+    sha256 "3542ec0838793e61d6224e27ff05e8ce4ba5a5c5cc4ec5c6a3e8d49247985477"
+  end
+
   def install
     # Dependencies
     ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
-    %w[pika pyliblo].each do |r|
+    %w[pika pyliblo pyserial].each do |r|
       resource(r).stage do
         system "python", *Language::Python.setup_install_args(libexec/"vendor")
       end
